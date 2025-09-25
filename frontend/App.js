e43a9c9d438602faf2f063eabdcb28b543ae9a8a
Premapriya <premapriya1905@gmail.com>
2025-09-25 14:55:47 +0530

    ProductList

--- frontend/App.js
+++ frontend/App.js
@@ -6,6 +6,7 @@ import AddProductScreen from "./components/screens/AddProductScreen";
 import AddUserScreen from "./components/screens/AddUserScreen";
 import ProductListScreen from "./components/screens/ProductListScreen";
 import UserFavoritesScreen from "./components/screens/UserFavoritesScreen";
+import ProductDetailScreen from "./components/screens/ProductDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,13 @@ export default function App() {
         <Stack.Screen name="AddUser" component={AddUserScreen} />
         <Stack.Screen name="ProductList" component={ProductListScreen} />
         <Stack.Screen name="UserFavorites" component={UserFavoritesScreen} />
+        <Stack.Screen 
+          name="ProductDetail" 
+          component={ProductDetailScreen}
+          options={({ route }) => ({
+            title: route.params?.product?.name || 'Product Details'
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
